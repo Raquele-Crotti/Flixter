6c1fa4fa206ce88b0bb75d967155a46f67cdb9f0
Raquele Crotti <raquelecrotti@gmail.com>
2019-08-16 03:06:21 +0000

    Added Hero section to homepage and styled it using Bootstrap and  CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-//= require jquery3
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
+//= require_tree .
