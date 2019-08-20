c67796598fe6b32743ea94ba9469f6c18ff13517
Raquele Crotti <raquelecrotti@gmail.com>
2019-08-20 07:53:12 +0000

    Added validations to courses in app/models/course.rb and app/controller/instructor/courses_controller.rb

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -7,7 +7,15 @@ class Instructor::CoursesController < ApplicationController
 
   def create
     @course = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
   end
 
   private
