54caaaa1644ba165265d9bf7fae9b3a203875def
Raquele Crotti <raquelecrotti@gmail.com>
2019-09-06 06:28:13 +0000

    Set up Stripe to process caredit/debit card/apple payments on app. Set Stripe API keys to application.yml and heroku (Application currently running in test mode)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,30 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_course, only: [:show]
+
+
+
   def show
   end
-
+  
   private
 
-  helper_method :current_lesson
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
+  
+
+  def require_enrolled_in_current_course
+    if current_user.enrolled_in?(current_lesson.section.course)
+    else
+      redirect_to course_path(current_course), alert: 'Need to Enroll'
+    end
   end
+
+  helper_method :current_lesson
+    def current_lesson
+      @current_lesson ||= Lesson.find(params[:id])
+    end
+    
+  helper_method :current_course
+    def current_course
+      @current_course ||= Course.find(params[:id])
+    end
 end
